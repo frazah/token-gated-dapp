751a95d0a8d9ea15dfe5fdf52ddd1c0a58458a5f
francesco <55762612+frazah@users.noreply.github.com>
2026-02-13 15:33:08 +0100

    Modified comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,9 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    // Rimuoviamo la configurazione di mining che non è più necessaria
     hardhat: {
-      chainId: 1337 // Usiamo un chainId standard per coerenza
+      chainId: 1337
     },
-    // Configuriamo localhost per puntare a Ganache
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 1337
